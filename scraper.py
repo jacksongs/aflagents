d022853a9d4d9233f4aa21206e934e9a1604355a
Jackson Gothe-Snape <jacksongs@gmail.com>
2015-06-09 23:24:08 +1000

    Minor update

--- scraper.py
+++ scraper.py
@@ -12,11 +12,11 @@ agentps = agentsoup.find_all("p")
 
 data = []
 for i, a in enumerate(agentps):
-	if len(a.find_all("br"))>3:
-		if a.find_all("br")[0].previousSibling.strip() == 'New South Wales':
-			data.append({"id": i, name": a.find_all("br")[1].nextSibling.strip(), "company": a.find_all("br")[2].nextSibling.strip(), "location": a.find_all("br")[-1].nextSibling.strip().replace(u'\xa0', u' ')}) 
-		else:
-			data.append({"id": i, "name": a.find_all("br")[0].previousSibling.strip(), "company": a.find_all("br")[1].previousSibling.strip(), "location": a.find_all("br")[-1].nextSibling.strip().replace(u'\xa0', u' ')}) 
-		
+    if len(a.find_all("br"))>3:
+        if a.find_all("br")[0].previousSibling.strip() == 'New South Wales':
+            data.append({"id": i, "name": a.find_all("br")[1].nextSibling.strip(), "company": a.find_all("br")[2].nextSibling.strip(), "location": a.find_all("br")[-1].nextSibling.strip().replace(u'\xa0', u' ')})
+        else:
+            data.append({"id": i, "name": a.find_all("br")[0].previousSibling.strip(), "company": a.find_all("br")[1].previousSibling.strip(), "location": a.find_all("br")[-1].nextSibling.strip().replace(u'\xa0', u' ')}) 
+        
 
 scraperwiki.sqlite.save(unique_keys=["name","company"], data=data, table_name='agents')
